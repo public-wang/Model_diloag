7f27111d9b91706f393c91d83daddee7309970f1
public wang <631940441@qq.com>
2016-09-24 19:06:52 +0800

    delete the whole project

--- routes/vedio.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET vedio page. */
-router.get('/', function(req, res, next) {
-  res.render('vedio', { title: 'Express' });
-});
-
-module.exports = router;
